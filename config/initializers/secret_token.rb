a9d5004fe459c51da1d1e814a3ccbfc65cdc1647
FinWin <finwin2002@msn.com>
2012-03-10 19:00:09 +0100

    primo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '211bf9d95d4cd997e368b46e87c91873e59fb10bf43cef7c3f1e0bdfc383735380b7019039cfabee2f8dc6f56e56f1a69e4ed2fd899faa35a4429e81de0d384b'
